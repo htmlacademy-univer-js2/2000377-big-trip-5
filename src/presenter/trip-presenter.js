242a81d488e95c46f8ec75cad33da3e1ac0cebef
Lehinskiy05 <peskovatskiy.leha@mail.ru>
2025-04-03 21:45:39 +0500

    2.15. Шаблонизируй это

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,29 +1,32 @@
 import { render } from '../render.js';
 import FilterView from '../view/filter-view.js';
 import SortView from '../view/sort-view.js';
-import AddEventView from '../view/add-event-view.js';
-import EditEventView from '../view/edit-event-view.js';
+import AddEditEventView from '../view/add-edit-event-view.js';
 import RouteSpotView from '../view/trip-event-view.js';
 import EventList from '../view/event-list-view.js';
-
+import RoutePointsModel from '../model/route-point-model.js';
 
 export default class TripPresenter {
   constructor() {
     this.eventsListContainer = new EventList;
     this.filterContainer = document.querySelector('.trip-controls__filters');
     this.eventsContainer = document.querySelector('.trip-events');
+    this.routePointsModel = new RoutePointsModel();
   }
 
   init() {
+    const routePoints = this.routePointsModel.getRoutePoints();
+
     render(new FilterView(), this.filterContainer);
     render(new SortView(), this.eventsContainer);
     render(this.eventsListContainer, this.eventsContainer);
-    render(new EditEventView(), this.eventsListContainer.getElement());
+    render(new AddEditEventView(routePoints[9]), this.eventsListContainer.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new RouteSpotView(), this.eventsListContainer.getElement());
-    }
-
-    render(new AddEventView(), this.eventsListContainer.getElement());
+    routePoints.forEach((routePoint) => {
+      if (routePoint.city) {
+        render(new RouteSpotView(routePoint), this.eventsListContainer.getElement());
+        render(new AddEditEventView(routePoint), this.eventsListContainer.getElement());
+      }
+    });
   }
 }
